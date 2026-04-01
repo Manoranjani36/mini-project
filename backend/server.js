afeda77ba87942e82eb6e28ca575043db0f3cd7c
Manoranjani36 <manoranjani9943@gmail.com>
2026-04-01 23:36:40 +0530

    Add missing import for 'path' module

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import dotenv from "dotenv";
 import { createRequire } from "module";
 import { fileURLToPath } from "url";
 
+
 const require = createRequire(import.meta.url);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
